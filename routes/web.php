0376e0cdfa9844cf1ca7a02624f7aa6827e7be09
nde95 <ndennison268@gmail.com>
2024-02-26 13:50:14 -0500

    dead default routes removed

--- routes/web.php
+++ routes/web.php
@@ -28,15 +28,3 @@ Route::get('/listings/{listing}', function (Listing $listing) {
         'listing' => $listing
     ]);
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
