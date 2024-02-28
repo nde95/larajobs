f42dfb8cd0d9a648f15a57e5d7b24e811c381398
nde95 <ndennison268@gmail.com>
2024-02-28 17:18:10 -0500

    manage page with all active user listings and edit/delete

---
+++ resources/views/listings/manage.blade.php
@@ -0,0 +1,61 @@
+<x-layout>
+    <x-card class="bg-gray-50 border border-gray-200 p-10 rounded">
+        <header>
+            <h1
+                class="text-3xl text-center font-bold my-6 uppercase"
+            >
+                Manage Listings
+            </h1>
+        </header>
+
+        <table class="w-full table-auto rounded-sm">
+            @unless($listings->isEmpty())
+            @foreach ($listings as $listing)
+            <tbody>
+                <tr class="border-gray-300">
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        <a href="/listings/{{$listing->id}}">
+                            {{ $listing->title }}
+                        </a>
+                    </td>
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        <a
+                            href="/listings/{{$listing->id}}/edit"
+                            class="text-blue-400 px-6 py-2 rounded-xl"
+                            ><i
+                                class="fa-solid fa-pen-to-square"
+                            ></i>
+                            Edit</a
+                        >
+                    </td>
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        <form action="/listings/{{$listing->id}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button class="text-red-600">
+                                <i
+                                    class="fa-solid fa-trash-can"
+                                ></i>
+                                Delete
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+                @else
+                <tr class="border-gray-300">
+                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                        <p class="text-center">No listings found</p>
+                    </td>
+                </tr>
+                @endunless
+            </tbody>
+        </table>
+    </x-card>
+</x-layout>
